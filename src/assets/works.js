c80c9f7496fc1bb2661e2dc83351e39a0c12e144
exalted100 <harounayomikun@gmail.com>
2022-09-12 01:37:58 +0100

    feat: added new work, which is the URL shortener service

--- src/assets/works.js
+++ src/assets/works.js
@@ -11,6 +11,12 @@ const works = [
     description:
       "A data visualization project, built with Apex Charts, React, and tghe Coin Gecko API, that shows the price history of cryptocurrencies across a period of days. The supported coins include Bitcoin, Litecoin, Ethereum, and Binance Coin. It also has functionality to choose the number of days the data should span, the last day the chart should show, and the coin for the data.",
   },
+  {
+    title: "03. URL Shortener Service",
+    link: "https://github.com/Exalted100/url-shortener",
+    description:
+      "A URL shortener service written in Go and using Redis as a store (for the urls).",
+  },
 ];
 
 export default works;
