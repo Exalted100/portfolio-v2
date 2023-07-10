7f38cf9e5c00255d416aa362eb71544a6d7bc708
exalted100 <harounayomikun@gmail.com>
2023-07-10 21:14:46 +0100

    updated Fisayo's portfolio link

--- src/assets/works.js
+++ src/assets/works.js
@@ -3,7 +3,7 @@ const works = [
     title: "01. Learn Nigerian Law",
     link: "https://www.learnnigerianlaw.com/",
     description:
-      "I built a website with comprehensive notes on the Nigerian law. I worked with <a href='https://mirrorfisayo.wixsite.com/my-site' target='_blank' rel='noopener noreferrer' class='referrals'>Fisayo</a> and <a href='https://gabrielabubakar.netlify.app/' target='_blank' rel='noopener noreferrer' class='referrals'>Gabriel</a>. My role involved project/product management and frontend development.",
+      "I built a website with comprehensive notes on the Nigerian law. I worked with <a href='https://mirrorfisayo.vzy.io/' target='_blank' rel='noopener noreferrer' class='referrals'>Fisayo</a> and <a href='https://gabrielabubakar.netlify.app/' target='_blank' rel='noopener noreferrer' class='referrals'>Gabriel</a>. My role involved project/product management and frontend development.",
   },
   {
     title: "02. Cryptocurrency Chart",
